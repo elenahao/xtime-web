079b9056546be527fd18de47a6eb48fbc4820794
郝雪颖 <haoxueying@haoxueyingdeMBP.lan>
2019-06-16 22:46:55 +0800

    路由配置,系统管理界面

--- src/router/components/portal/personInfo.js
+++ src/router/components/portal/personInfo.js
@@ -1,9 +1,39 @@
 export default [{
-        path: "password/reset",
-        component: () => import( /* webpackChunkName: "personInfo" */ '@/views/portal/Password')
+        path: "password",
+        name: "修改密码",
+        component: () => import( /* webpackChunkName: "personInfo" */ '@/views/portal/Password'),
+        meta: {
+            title: "breadcrumb",
+            breadcrumb: [
+                {
+                    name: "平台系统",
+                },
+                {
+                    name: "基础信息",
+                },
+                {
+                    name: "修改密码",
+                }
+            ]
+        }
     },
     {
-        path: "data/list",
-        component: () => import( /* webpackChunkName: "personInfo" */ '@/views/portal/UserData')
+        path: "data",
+        name: "个人信息",
+        component: () => import( /* webpackChunkName: "personInfo" */ '@/views/portal/UserData'),
+        meta: {
+            title: "breadcrumb",
+            breadcrumb: [
+                {
+                    name: "平台系统",
+                },
+                {
+                    name: "基础信息",
+                },
+                {
+                    name: "个人信息",
+                }
+            ]
+        }
     }
 ]
--- src/views/portal/System.vue
+++ src/views/portal/System.vue
@@ -1,10 +1,70 @@
 <template>
     <div>
-        <h1>系统管理</h1>
+        <el-form :inline="true" :model="form" class="demo-form-inline">
+            <el-form-item label="系统名称">
+                <el-input v-model="form.name" placeholder="系统名称"></el-input>
+            </el-form-item>
+            <el-form-item label="系统编码">
+                <el-input v-model="form.code" placeholder="系统编码"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">查询</el-button>
+            </el-form-item>
+        </el-form>
+        <el-table :data="tableData" stripe border>
+            <el-table-column prop="id" label="ID" width="100"></el-table-column>
+            <el-table-column prop="name" label="系统名称" width="180"></el-table-column>
+            <el-table-column prop="code" label="系统编码" width="180"></el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button @click="handleClick(scope.row)" type="text" size="medium">编辑</el-button>
+                    <el-button type="text" size="medium">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
-</template>
+</template> 
+
 <script>
 export default {
-    name: "system"
-}
+    name: "system",
+    data() {
+        return {
+            form: {
+                name: "",
+                code: ""
+            },
+            tableData: [
+                {
+                    id: "1",
+                    name: "平台系统",
+                    code: "portal"
+                },
+                {
+                    id: "2",
+                    name: "核心系统",
+                    code: "cmc"
+                },
+                {
+                    id: "3",
+                    name: "票务系统",
+                    code: "ticket"
+                },
+                {
+                    id: "4",
+                    name: "智能排片系统",
+                    code: "ifs"
+                }
+            ]
+        };
+    },
+    methods: {
+        onSubmit() {
+            console.log("submit!");
+        },
+        handleClick(row) {
+            console.log(row);
+        }
+    }
+};
 </script>

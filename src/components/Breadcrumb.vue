9677d2a1ae45d9cd24095fa4ae0d4bf46329fa69
xueying.hao <xueying.hao@mtime.com>
2019-06-20 15:34:07 +0800

    登录，面包屑

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,18 +1,40 @@
 <template>
     <el-breadcrumb class="breadcrumb" separator="/">
-        <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index">{{item.name}}</el-breadcrumb-item>
+        <el-breadcrumb-item v-for="(item, index) in this.breadcrumbList" :key="index">{{item}}</el-breadcrumb-item>
     </el-breadcrumb>
 </template>
 <script>
+import { mapState, mapMutations } from 'vuex'
 export default {
     data() {
         return {
-            breadcrumbList: this.$router.history.current.meta.breadcrumb
+            // breadcrumbList: this.breadcrumbList
         }
+    },
+    computed: {
+        ...mapState('global', ['breadcrumbList', 'sidebarList'])
+    },
+    methods: {
+        ...mapMutations('global', ['changeBreadcrumbList'])
     },
     watch: {
         $route() {
-            this.breadcrumbList = this.$router.history.current.meta.breadcrumb
+            console.log("==="+this.$router.history.current.path);
+            // this.breadcrumbList = this.$router.history.current.meta.breadcrumb
+            const router = this.$router.history.current.path
+            for (const item of this.sidebarList) {
+                if (router === item.router) {
+                    // this.sidebarActive = item.index
+                    this.changeBreadcrumbList(item.breadcrumbList);
+                } else {
+                    for (const cItem of item.children) {
+                        if (router === cItem.router) {
+                            // this.sidebarActive = cItem.index
+                            this.changeBreadcrumbList(cItem.breadcrumbList);
+                        }
+                    }
+                }
+            }
         }
     },
 

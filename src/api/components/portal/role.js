762dfcc695a9234dba9af542abcd67f14fd9ced3
郝雪颖 <haoxueying@haoxueyingdeMBP.lan>
2019-06-23 23:42:37 +0800

    角色页面

---
+++ src/api/components/portal/role.js
@@ -0,0 +1,42 @@
+import api from "@/api"
+
+export const dialogSubmit = (data) => {
+    return api.request({
+        url: `/api/role/createOrUpdate`,
+        method: 'put',
+        params: data
+    })
+}
+
+export const deleteSubmit = (data) => {
+    return api.request({
+        url: `/api/role/deleteById`,
+        method: 'delete',
+        params: data
+    })
+}
+
+export const listSubmit = (data) => {
+    return api.request({
+        url: `/api/role/list`,
+        method: 'post',
+        params: data
+    })
+}
+
+export const findRoleTransferSubmit = (data) => {
+    return api.request({
+        url: `/api/role/findRoleByUserCode`,
+        method: 'post',
+        params: data
+    })
+}
+
+export const saveUserRoleSubmit = (data) => {
+    return api.request({
+        url: `/api/role/saveUserRole`,
+        method: 'post',
+        data: data
+    })
+}
+

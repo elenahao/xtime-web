9d0d31846b147724b1dd4702fa27442b6345f356
xueying.hao <xueying.hao@mtime.com>
2019-06-26 19:19:40 +0800

    菜单

--- src/views/portal/Menu.vue
+++ src/views/portal/Menu.vue
@@ -1,7 +1,30 @@
 <template>
     <div>
         <el-button class="btn-creat" type="success" icon="el-icon-plus" @click="handleCreate">创建</el-button>
-        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        <!-- <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree> -->
+        <el-tree
+        :data="menuData"
+        node-key="id"
+        default-expand-all
+        :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+            <span>{{ node.label }}</span>
+            <span>
+            <el-button
+                type="text"
+                size="mini"
+                @click="() => edit(data)">
+                编辑
+            </el-button>
+            <el-button
+                type="text"
+                size="mini"
+                @click="() => remove(node, data)">
+                删除
+            </el-button>
+            </span>
+        </span>
+        </el-tree>
         <el-dialog :title="titleName" :visible.sync="dialogFormVisible" width="30%">
             <el-form :model="dialogForm">
                 <el-input v-model="dialogForm.id" type="hidden"></el-input>
@@ -10,6 +33,19 @@
                 </el-form-item>
                 <el-form-item label="菜单名称" :label-width="formLabelWidth">
                     <el-input v-model="dialogForm.menuName" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="父级菜单" :label-width="formLabelWidth">
+                     <el-cascader
+                        v-model="value"
+                        :options="menuData"
+                        :props="{ expandTrigger: 'hover', checkStrictly: true }"
+                        @change="handleChange" clearable></el-cascader>
+                </el-form-item>
+                <el-form-item label="菜单URL" :label-width="formLabelWidth">
+                    <el-input v-model="dialogForm.menuUrl" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="排序码" :label-width="formLabelWidth">
+                    <el-input v-model="dialogForm.rank" autocomplete="off"></el-input>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
@@ -20,63 +56,71 @@
     </div>
 </template>
 <script>
+import * as Menu from '@/api/components/portal/menu.js'
 export default {
     data() {
       return {
+          value:[],
+        menuData: [],
         dialogForm: {
             id: 0,
             menuCode: "",
-            menuName: ""
+            menuName: "",
+            pMenuCode: "",
+            menuUrl: "",
+            rank: "",
+            sysCode: "",
+            level: ""
         },
         dialogFormVisible: false,
         formLabelWidth: '100px',
         titleName: "创建菜单",
         buttonName: "创建",
-        data: [{
-          label: '一级 1',
-          children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
-          }]
-        }, {
-          label: '一级 2',
-          children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
-          }]
-        }, {
-          label: '一级 3',
-          children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
-          }, {
-            label: '二级 3-2',
-            children: [{
-              label: '三级 3-2-1'
-            }]
-          }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
-      };
+      }
     },
     methods: {
+        async getMenuData(){
+            try {
+                const res = await Menu.getMenuDataSubmit({});
+                console.log(res);
+                this.menuData = res.data;
+            } catch (error) {
+                console.log(error);
+            }
+        },
       handleNodeClick(data) {
         console.log(data);
       },
+      handleChange(value) {
+        console.log(value);
+      },
+      async handleDialogSubmit() {
+          this.dialogFormVisible = false
+          this.dialogForm.sysCode = this.value.get(0)
+          this.dialogForm.level = this.value.length
+          this.dialogForm.pMenuCode = this.value.get(this.value.length-1)
+          try {
+                const res = await Menu.dialogSubmit({
+                    id: this.dialogForm.id,
+                    code: this.dialogForm.menuCode,
+                    name: this.dialogForm.menuName,
+                    pCode: this.dialogForm.pMenuCode,
+                    menuUrl: this.dialogForm.menuUrl,
+                    rank: this.dialogForm.rank,
+                    sysCode: this.dialogForm.sysCode,
+                    level: this.dialogForm.level
+                })
+                if(res.data != 0){
+                    this.$message({
+                    message: '恭喜您，'+ this.buttonName + "成功",
+                    type: 'success'
+                  })
+                }
+                this.getList()
+            } catch (error) {
+                console.log(error)
+            }
+        },
       handleCreate(){
         this.dialogFormVisible = true;
         this.titleName = "创建菜单";
@@ -84,7 +128,25 @@ export default {
         this.dialogForm.id = 0;
         this.dialogForm.userCode = "";
         this.dialogForm.username = "";
+      },
+      edit(data) {
+        // const newChild = { id: id++, label: 'testtest', children: [] };
+        // if (!data.children) {
+        //   this.$set(data, 'children', []);
+        // }
+        // data.children.push(newChild);
+        console.log(data)
+      },
+
+      remove(node, data) {
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === data.id);
+        children.splice(index, 1);
       }
+    },
+    created() {
+        this.getMenuData()
     }
   };
 </script>
